9d72a6b23017ce3f67c7783bccbaf332ad1a14ea
cjtim <iam@cjtim.com>
2022-01-02 11:58:57 +0700

    ci: fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 TMP_FILE=tmp_hosts.yml
+TMP_SSH=tmp_id_rsa
 
 if [ -z "$ANSIBLE_INVENTORY_BASE64" ]; then
     echo "Please set ANSIBLE_INVENTORY_BASE64 variable"
@@ -10,9 +11,11 @@ if [ -z "$ANSIBLE_INVENTORY_BASE64" ]; then
 fi
 
 echo "$ANSIBLE_INVENTORY_BASE64" | base64 -d > $TMP_FILE
+echo "$SSH_KEY" > $TMP_SSH
+chmod 600 $TMP_SSH
 
 # run playbook
 ls playbooks | xargs -I {} ansible-playbook playbooks/{} --inventory=$TMP_FILE
 
 # cleanup
-rm -f $TMP_FILE
+rm -f $TMP_FILE $TMP_SSH
