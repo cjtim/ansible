cae24d65f3d2d3c3cd66d64f4795db055b53480f
cjtim <iam@cjtim.com>
2022-01-02 23:21:21 +0700

    add install docker-compose

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-TMP_FILE=tmp_hosts.yml
-TMP_SSH=tmp_id_rsa
+DEFAULT_ID_RSA_PATH=~/.ssh/id_rsa
+DEFAULT_ID_RSA_PUB_PATH=~/.ssh/id_rsa.pub
+
+TMP_INVENTORY_FILE=tmp_hosts.yml
+TMP_SSH_FILE=tmp_id_rsa
 
 if [ -z "$ANSIBLE_INVENTORY_BASE64" ]; then
     echo "Please set ANSIBLE_INVENTORY_BASE64 variable"
@@ -9,17 +12,26 @@ if [ -z "$ANSIBLE_INVENTORY_BASE64" ]; then
     echo 'ANSIBLE_INVENTORY_BASE64=$(base64 < hosts.yml) ./deploy.sh'
     exit 1
 fi
+echo "$ANSIBLE_INVENTORY_BASE64" | base64 -d > $TMP_INVENTORY_FILE
 
+# lookup for private key and store in file
+# set properly permission 
+if [ -z "$SSH_KEY" ]; then
+    SSH_KEY=$(cat $DEFAULT_ID_RSA_PATH)
+    export SSH_KEY
+fi
+echo "$SSH_KEY" > $TMP_SSH_FILE
+chmod 600 $TMP_SSH_FILE
+
+# lookup for public key and store in var
 if [ -z "$SSH_PUB_KEY" ]; then
-    export SSH_PUB_KEY=$(cat ~/.ssh/id_rsa.pub)
+    SSH_PUB_KEY=$(cat $DEFAULT_ID_RSA_PUB_PATH)
+    export SSH_PUB_KEY
 fi
 
-echo "$ANSIBLE_INVENTORY_BASE64" | base64 -d > $TMP_FILE
-echo "$SSH_KEY" > $TMP_SSH
-chmod 600 $TMP_SSH
 
 # run playbook
-ls playbooks | xargs -I {} ansible-playbook playbooks/{} --inventory=$TMP_FILE
+ls playbooks | xargs -I {} ansible-playbook playbooks/{} --inventory=$TMP_INVENTORY_FILE
 
 # cleanup
-rm -f $TMP_FILE $TMP_SSH
+rm -f $TMP_INVENTORY_FILE $TMP_SSH_FILE
